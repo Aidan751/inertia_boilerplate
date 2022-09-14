72579c119ec361d8e2bee6da0be13ac44162fd9f
aidanClark1 <aidanclark56@gmail.com>
2022-09-14 14:08:59 +0100

    Merge branch

--- resources/js/Components/SideBar.js
+++ resources/js/Components/SideBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Head, Link } from '@inertiajs/inertia-react'
 import { usePage } from '@inertiajs/inertia-react'
+import AdminSideBar from '@/Partials/AdminSideBar';
 
 
 export default function SideBar({ user }) {
@@ -10,7 +11,7 @@ export default function SideBar({ user }) {
             <div class="row justify-content-center">
                 <div class="col-sm-3 sidebar col-fixed">
 
-                    
+                    <AdminSideBar />
 
                 </div>
             </div>
