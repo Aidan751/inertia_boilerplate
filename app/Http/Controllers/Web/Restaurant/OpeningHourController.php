758d8c8fab9d35aa6e66f2375fe8bdb21693159f
aidanClark1 <aidanclark56@gmail.com>
2022-10-07 10:23:50 +0100

    edit in openinhour controller complete

--- app/Http/Controllers/Web/Restaurant/OpeningHourController.php
+++ app/Http/Controllers/Web/Restaurant/OpeningHourController.php
@@ -12,12 +12,38 @@ class OpeningHourController extends Controller
     public function edit()
     {
         // get the opening times for the restaurant
-        $opening_hours = OpeningHour::where('restaurant_id', Auth::user()->restaurant->id)->get();
+        $opening_hours_monday = OpeningHour::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 4)->get();
+        $opening_hours_tuesday = OpeningHour::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 6)->get();
+        $opening_hours_wednesday = OpeningHour::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 1)->get();
+        $opening_hours_thursday = OpeningHour::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 5)->get();
+        $opening_hours_friday = OpeningHour::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 3)->get();
+        $opening_hours_saturday = OpeningHour::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 7)->get();
+        $opening_hours_sunday = OpeningHour::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 2)->get();
+
         // get the collection times for the restaurant
-        $collection_times = CollectionTime::where('restaurant_id', Auth::user()->restaurant->id)->get();
+        $collection_times_monday = CollectionTime::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 4)->get();
+        $collection_times_tuesday = CollectionTime::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 6)->get();
+        $collection_times_wednesday = CollectionTime::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 1)->get();
+        $collection_times_thursday = CollectionTime::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 5)->get();
+        $collection_times_friday = CollectionTime::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 3)->get();
+        $collection_times_saturday = CollectionTime::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 7)->get();
+        $collection_times_sunday = CollectionTime::where('restaurant_id', Auth::user()->restaurant->id)->where('day_id', 2)->get();
+
         return Inertia::render('Restaurant/OpeningTimes/Edit', [
-            'opening_hours' => $opening_hours,
-            'collection_times' => $collection_times,
+            'opening_hours_monday' => $opening_hours_monday,
+            'opening_hours_tuesday' => $opening_hours_tuesday,
+            'opening_hours_wednesday' => $opening_hours_wednesday,
+            'opening_hours_thursday' => $opening_hours_thursday,
+            'opening_hours_friday' => $opening_hours_friday,
+            'opening_hours_saturday' => $opening_hours_saturday,
+            'opening_hours_sunday' => $opening_hours_sunday,
+            'collection_times_monday' => $collection_times_monday,
+            'collection_times_tuesday' => $collection_times_tuesday,
+            'collection_times_wednesday' => $collection_times_wednesday,
+            'collection_times_thursday' => $collection_times_thursday,
+            'collection_times_friday' => $collection_times_friday,
+            'collection_times_saturday' => $collection_times_saturday,
+            'collection_times_sunday' => $collection_times_sunday,
         ]);
     }
 
