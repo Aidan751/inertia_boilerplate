f739f3196e129998db41c95cc18da3fcffd86d42
aidanClark1 <aidanclark56@gmail.com>
2022-10-21 00:34:12 +0100

    adjusted seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ use App\Models\MenuCategory;
 use Illuminate\Database\Seeder;
 use App\Models\RestaurantCategory;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -26,10 +27,37 @@ class DatabaseSeeder extends Seeder
         $this->call(LaratrustSeeder::class);
         // create 7 days
         Day::factory()->count(7)->create();
-        RestaurantCategory::factory(10)->create();
+        RestaurantCategory::factory()->create([
+            'name' => 'Italian',
+        ]);
+        RestaurantCategory::factory()->create([
+            'name' => 'Chinese',
+        ]);
+        RestaurantCategory::factory()->create([
+            'name' => 'Indian',
+        ]);
+        RestaurantCategory::factory()->create([
+            'name' => 'Fine Dining',
+        ]);
+        RestaurantCategory::factory()->create([
+            'name' => 'Fast Food',
+        ]);
+
+
+
+        // $table->string('banner')->nullable();
+        // $table->string('logo')->nullable();
         $restaurant = \App\Models\Restaurant::factory()->create([
             'name' => 'Test Restaurant',
             'contact_number' => '+447368360039',
+            'front_facing_number' => '+447368360039',
+            'address_line_1' => '26 Wilson Street',
+            'town' => 'Larkhall',
+            'county' => 'South Lanarkshire',
+            'postcode' => 'ML9 2QF',
+            'application_status' => 'approved',
+            'stripe_status' => 'complete',
+            'logo' => 'https://loremflickr.com/320/240?random=1',
         ]);
 
         OpeningHour::factory()->create([
@@ -108,20 +136,120 @@ class DatabaseSeeder extends Seeder
         $user->attachRole($role);
 
 
+        MenuCategory::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'title' => 'Pizza',
+            'notes' => 'Pizza notes',
+        ]);
+        MenuCategory::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'title' => 'Pasta',
+            'notes' => 'Pasta notes',
+        ]);
+        MenuCategory::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'title' => 'Drinks',
+            'notes' => 'Drinks notes',
+        ]);
+        MenuCategory::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'title' => 'Desserts',
+            'notes' => 'Desserts notes',
+        ]);
+        MenuCategory::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'title' => 'Sides',
+            'notes' => 'Sides notes',
+        ]);
 
-        MenuCategory::factory(50)->create([
-            "restaurant_id" => $restaurant->id,
+
+        MenuItem::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'menu_category_id' => 1,
+            'title' => 'Pizza',
+            'description' => 'Tomato sauce, mozzarella, basil',
+            'dietary_requirements' => 'Vegetarian',
+            'image' => "https://loremflickr.com/320/240?food&random=5",
+            'price' => 5.00,
         ]);
-        MenuItem::factory(50)->create();
-        Extra::factory(50)->create([
-            "restaurant_id" => $restaurant->id,
+        MenuItem::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'menu_category_id' => 2,
+            'title' => 'Pasta',
+            'description' => 'Tomato sauce, mozzarella, basil',
+            'dietary_requirements' => 'Vegetarian',
+            'image' => "https://loremflickr.com/320/240?food&random=3",
+            'price' => 5.00,
         ]);
-        GroupDeal::factory(50)->create([
-            "restaurant_id" => $restaurant->id,
+
+        MenuItem::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'menu_category_id' => 3,
+            'title' => 'Coke',
+            'description' => 'Coke',
+            'dietary_requirements' => 'none',
+            'image' => "https://loremflickr.com/320/240?food&random=2",
+            'price' => 10.00,
         ]);
+
+        MenuItem::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'menu_category_id' => 4,
+            'title' => 'Ice Cream',
+            'description' => 'Ice Cream',
+            'dietary_requirements' => 'none',
+            'image' => "https://loremflickr.com/320/240?food&random=1",
+            'price' => 15.00,
+        ]);
+
+        Extra::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'name' => 'Extra Cheese',
+            'description' => 'Extra Cheese',
+            'additional_charge' => 1.00,
+        ]);
+
+        Extra::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'name' => 'Extra Sauce',
+            'description' => 'Extra Sauce',
+            'additional_charge' => 1.00,
+        ]);
+
+        Extra::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'name' => 'Extra Pepperoni',
+            'description' => 'Extra Pepperoni',
+            'additional_charge' => 1.00,
+        ]);
+
+        Extra::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'name' => 'Extra Pineapple',
+            'description' => 'Extra Pineapple',
+            'additional_charge' => 1.00,
+        ]);
+
+        Extra::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'name' => 'Extra Mushrooms',
+            'description' => 'Extra Mushrooms',
+            'additional_charge' => 1.00,
+        ]);
+
+
+
+        GroupDeal::factory()->create([
+            'restaurant_id' => $restaurant->id,
+            'title' => 'Pizza Deal',
+            'description' => 'Pizza Deal',
+            'group_deal_price' => 10.00,
+        ]);
+
         Order::factory(50)->create([
             "restaurant_id" => $restaurant->id,
         ]);
+
         OrderItem::factory(100)->create([
         ]);
     }
