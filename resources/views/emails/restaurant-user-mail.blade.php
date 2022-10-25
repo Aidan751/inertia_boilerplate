ace62274272cf4847df1e468003f563859134356
aidanClark1 <aidanclark56@gmail.com>
2022-10-25 14:35:29 +0100

    mail testing works

--- resources/views/emails/restaurant-user-mail.blade.php
+++ resources/views/emails/restaurant-user-mail.blade.php
@@ -1,4 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <title>Welcome User</title>
+</head>
+<body>
 
+<div class="p-5">
     <p>Hi {{ $details['first_name'] }} {{ $details['last_name'] }},</p>
 
     <p>A new account has been setup for you for the Order It platform, your login details are listed below.</p>
@@ -10,3 +21,9 @@
 
     <p>You can access your account at:</p>
     <a href={{config('app.url') . "/login"}}>{{config('app.url') . "/login"}}</a>
+</div>
+</body>
+</html>
+
+
+
