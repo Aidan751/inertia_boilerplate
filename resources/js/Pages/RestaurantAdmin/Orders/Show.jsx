b4a930a3f75f24ec5f9b4072aa8c1a5b4f380b2c
aidanClark1 <aidanclark56@gmail.com>
2022-11-03 09:11:14 +0000

    adding buttons and textarea to order show

--- resources/js/Pages/RestaurantAdmin/Orders/Show.jsx
+++ resources/js/Pages/RestaurantAdmin/Orders/Show.jsx
@@ -24,7 +24,9 @@ console.log(props);
 
 
                 {/* Page Content */}
-                <main className="col-span-12 px-10">
+
+
+                <main className="col-span-12 px-5">
           {/* BEGIN: Invoice */}
           <div className="intro-y box overflow-hidden mt-5">
             <div className="border-b border-slate-200/60 dark:border-darkmode-400 text-center sm:text-left">
@@ -34,11 +36,11 @@ console.log(props);
                   {props.order.pickup_date} - {props.order.order_reference}
                 </div>
 
-                <div className="mt-3 text-lg">View Order</div>
+                <div className="mt-3">View Order</div>
               </div>
               <div className="flex flex-col lg:flex-row px-5 sm:px-20 pt-10 pb-10 sm:pb-20">
                 <div>
-                  <div className="text-lg font-medium text-slate-500 mb-5">
+                  <div className="text-md font-medium text-slate-500 mb-5">
                     Customer Details
                   </div>
                   <div className="text-md font-medium">Name</div>
@@ -74,7 +76,7 @@ console.log(props);
               </div>
             </div>
             <div className="px-5 sm:px-16 py-10 sm:py-20">
-              <div className="mt-0 mb-5 font-medium text-lg pl-5">
+              <div className="mt-0 mb-5 font-medium text-md pl-5">
                 Order Details
               </div>
 
@@ -109,42 +111,85 @@ console.log(props);
                         </td>
                       </tr>
                     ))}
-                    <tr className="border-t-2 border-b-2 border-black-300 dark:border-darkmode-400 py-5">
-                      <td className="dark:border-darkmode-400">
-                        <div className="font-medium text-primary text-lg whitespace-nowrap">
+                    <tr>
+                      <td className="border-b border-x-4 dark:border-darkmode-400">
+                        <div className="font-medium text-primary whitespace-nowrap">
                           Delivery
                         </div>
                       </td>
-                      <td className="text-right dark:border-darkmode-400 w-32">
+                      <td className="text-right border-b dark:border-darkmode-400 w-32">
                         {" "}
                       </td>
-                      <td className="text-right text-primary text-lg dark:border-darkmode-400 w-32">
+                      <td className="text-right text-primary border-b dark:border-darkmode-400 w-32">
                         {props.order.delivery_price}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="border-b dark:border-darkmode-400">
+                        <div className="font-medium whitespace-nowrap">
+                          Total:
+                        </div>
+                      </td>
+                      <td className="text-right border-b dark:border-darkmode-400 w-32">
+                        {props.order_items.reduce(
+                          (prev, curr, index, array) => prev + curr.quantity,
+                          0
+                        )}
+                      </td>
+                      <td className="text-right border-b dark:border-darkmode-400 w-32">
+                        {props.order.price}
                       </td>
                     </tr>
                   </tbody>
                 </table>
               </div>
-            </div>
-            <div className="px-5 sm:px-20 pb-10 sm:pb-20 flex flex-col-reverse sm:flex-row">
-              <div className="text-center sm:text-left mt-10 sm:mt-0">
-                <div className="text-lg text-primary font-medium mt-2">
-                  Total:
-                </div>
-              </div>
 
-              <div className="text-center flex sm:text-right sm:ml-auto">
-              <div className="text-lg text-primary font-medium mt-2 mr-5">
-                  total quantity here
+              {/* Start: driver collection time and accept button */}
+              <div className="flex flex-wrap gap-6 justify-between items-end p-10 sm:p-10">
+                <div className="w-64 flex-1">
+                  <h3 className="mb-3">Enter driver collection time*</h3>
+                  <input
+                    className="w-full"
+                    type="time"
+                    min="00:00"
+                    max="23:00"
+                  />
                 </div>
-                <div className="text-xl text-primary font-medium mt-2">
-                  {props.order.price}
+                <button className="btn btn-primary w-64 flex-1 py-3">
+                  Accept and Complete
+                </button>
+              </div>
+              {/* End: driver collection time and accept button */}
+              {/* Start: decline button */}
+              <div className="flex justify-center">
+                <button className="btn btn-secondary">Decline</button>
+              </div>
+              {/* End: decline button */}
+              {/* Start: message to customer */}
+              <div className="mt-3">
+                <label>Message to customer</label>
+                <div className="mt-2">
+                  <textarea
+                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                    id="message_to_customer"
+                    type="text"
+                    name="message_to_customer"
+                  />
                 </div>
               </div>
+              {/* End: message to customer */}
+              {/* Start: send push notification */}
+              <div className="flex justify-end mt-8">
+                <button className="btn btn-primary">
+                  Send push notification
+                </button>
+              </div>
+              {/* End: send push notification */}
             </div>
           </div>
           {/* END: Invoice */}
         </main>
+
 
             </Authenticated>
         </>
